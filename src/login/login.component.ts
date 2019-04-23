d1dae40215b2b1aa1c9a65e658b092f0bdc31cc7
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-04-23 12:44:02 -0300

    teste

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef} from '@angular/
+import { MatDialog, MatDialogRef} from '@angular/material'
 
 @Component({
   selector: 'app-login',
