81b93907b95ca3435a12dcfc36d0806f0051c706
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-28 10:58:49 -0300

    form controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatListModule } from '@angular/material/list';
--- src/comments-form/comments-form.component.ts
+++ src/comments-form/comments-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-comments-form',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments-form.component.css']
 })
 export class CommentsFormComponent implements OnInit {
+  
+  commentsForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+    rate: ['', [Validators.required]],
+    comment: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(500)]]
+  })
 
-  constructor() { }
+  formErrors = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    telnum: ''
+  };
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit() {
   }
