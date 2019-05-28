cd1ee30fe0259355a33781883b31b6c88d19806b
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-28 11:29:48 -0300

    formValidation

--- src/comments-form/comments-form.component.ts
+++ src/comments-form/comments-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
@@ -8,20 +8,64 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 })
 export class CommentsFormComponent implements OnInit {
   
-  commentsForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
-    rate: ['', [Validators.required]],
-    comment: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(500)]]
-  })
+  commentsForm: FormGroup;
+
+  validationMessages = {
+    name: {
+      required: "First name is required",
+      minLength: "First name min length is 2 ccharacters",
+      maxLength: "First name max length is 50 characters"
+    },
+    rate: {
+      required: "A rate is required",
+    },
+    comment: {
+      required: "Please, leave a comment",
+      minLength: "Comment min length is 5 characters",
+      maxLength: "Comment max length is 500 characters"
+    }
+  }
 
   formErrors = {
-    firstname: '',
-    lastname: '',
-    email: '',
-    telnum: ''
+    name: '',
+    rate: '',
+    comment: '',
   };
-  constructor( private fb: FormBuilder ) { }
 
+  constructor( private fb: FormBuilder ) { 
+    this.createForm();
+  }
+
+  createForm() {
+    this.commentsForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      rate: ['', [Validators.required]],
+      comment: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(500)]]
+    });
+    this.commentsForm.valueChanges.subscribe( data => { this.onValueChanged(data)});
+    this.onValueChanged(); //reset form data
+
+  }
+
+  onValueChanged(data) {
+    if (!this.commentsForm) { return; }
+    const form = this.commentsForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
   ngOnInit() {
   }
 
