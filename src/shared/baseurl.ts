59287086f7455801a3a9a644a29ed9e351e9b0c2
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-31 14:19:42 -0300

    images and backend

--- src/shared/baseurl.ts
+++ src/shared/baseurl.ts
@@ -1,2 +1,2 @@
-export const baseImageURL = "http://firebasestorage.googleapis.com/v0/b/courseracourse-22cd9.appspot.com/o/";
+export const baseImageURL = "https://firebasestorage.googleapis.com/v0/b/courseracourse-22cd9.appspot.com/o/";
 export const databaseURL = "https://courseracourse-22cd9.firebaseio.com/";
